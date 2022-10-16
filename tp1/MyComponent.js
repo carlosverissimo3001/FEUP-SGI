a7c6a97d281dd78545a2acd89dfb955b0b64f7f8
Carlos Verissimo <up201907716@edu.fe.up.pt>
2022-10-16 20:01:16 +0100

    Views can now be changed through the interface

--- tp1/MyComponent.js
+++ tp1/MyComponent.js
@@ -1,7 +1,7 @@
 import { CGFobject } from "../lib/CGF.js";
 
 /**
- * MyNode
+ * MyComponenet
  * @method constructor
  * @param scene
  * @param componentID ID of the component (unique)
@@ -13,68 +13,77 @@ import { CGFobject } from "../lib/CGF.js";
  * @param length_s
  */
 
-export class MyComponent extends CGFobject{
-    constructor(scene, componentID, transf, materials, textureID, children, primtives, length_s, length_t) {
-        super(scene);
-        this.componentID = componentID;
-        this.transf = transf;
-        this.materials= materials;
-        this.textureID = textureID;
-        this.children = children;
-        this.primitives = primtives;
-        this.length_s = length_s;
-        this.length_t = length_t;
+export class MyComponent extends CGFobject {
+  constructor(
+    scene,
+    componentID,
+    transf,
+    materials,
+    textureID,
+    children,
+    primtives,
+    length_s,
+    length_t
+  ) {
+    super(scene);
+    this.componentID = componentID;
+    this.transf = transf;
+    this.materials = materials;
+    this.textureID = textureID;
+    this.children = children;
+    this.primitives = primtives;
+    this.length_s = length_s;
+    this.length_t = length_t;
 
-        this.materialID = this.materials[0];
-        this.matIndex = 0;
-    }
+    this.materialID = this.materials[0];
+    this.matIndex = 0;
+  }
 
-    changeMaterial(){
-        this.matIndex++;
+  changeMaterial() {
+    this.matIndex++;
 
-        if (this.matIndex == this.materials.length)
-            this.matIndex = 0
+    if (this.matIndex == this.materials.length) this.matIndex = 0;
 
-        this.materialID = this.materials[this.matIndex];
-    }
+    this.materialID = this.materials[this.matIndex];
+  }
 
-    setTransformation(transf) {
-        this.transf = transf;
-    }
+  setTransformation(transf) {
+    this.transf = transf;
+  }
 
-    setMaterial(material) {
-        this.material = material;
-    }
+  setMaterial(material) {
+    this.material = material;
+  }
 
-    setTexture(texture) {
-        this.texture = texture;
-    }
+  setTexture(texture) {
+    this.texture = texture;
+  }
 
-    setChildren(children) {
-        this.children = children;
-    }
+  setChildren(children) {
+    this.children = children;
+  }
 
-    setPrimitives(primitives) {
-        this.primitives = primitives;
-    }
+  setPrimitives(primitives) {
+    this.primitives = primitives;
+  }
 
-    getTransformation() {
-        return this.transf;
-    }
+  getTransformation() {
+    return this.transf;
+  }
 
-    getMaterial() {
-        return this.material;
-    }
+  getMaterial() {
+    return this.material;
+  }
 
-    getTexture() {
-        return this.texture;
-    }
+  getTexture() {
+    return this.texture;
+  }
 
-    getChildren() {
-        return this.children;
-    }
+  getChildren() {
+    return this.children;
+  }
 
-    getPrimitives() {
-        return this.primitives;
-    }
+  getPrimitives() {
+    return this.primitives;
+  }
 }
