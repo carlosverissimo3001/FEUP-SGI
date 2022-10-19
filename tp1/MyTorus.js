5ac7533d92897d271eff4aa6e59095214339dab8
Miguel <up201907756@edu.fe.up.pt>
2022-10-19 11:56:20 +0100

    Finish fix torus - aleluia

--- tp1/MyTorus.js
+++ tp1/MyTorus.js
@@ -27,43 +27,39 @@ export class MyTorus extends CGFobject{
       this.normals = [];
       this.texCoords = [];
 
-      var angle = 0;
-      var angleStep = Math.PI * 2 / this.loops;
-      var phi = 0;
-      var phiStep = Math.PI * 2 / this.slices;
 
       for (var loop = 0; loop <= this.loops; loop++) {
-          var cosAngle = Math.cos(angle);
-          var sinAngle = Math.sin(angle);
+
+          let outer_angle = loop /this.loops * 2 * Math.PI;
+          let outer_x = Math.cos(outer_angle) * this.outerRadius;
+          let outer_y = Math.sin(outer_angle) * this.outerRadius;
 
           for (var slice = 0; slice <= this.slices; slice++) {
-              var cosPhi = Math.cos(phi);
-              var sinPhi = Math.sin(phi);
+              let inner_angle = slice / this.slices * 2 * Math.PI;
 
-              this.vertices.push((this.outerRadius + this.innerRadius * cosAngle) * cosPhi, (this.outerRadius + this.innerRadius * cosAngle) * sinPhi, this.innerRadius * sinAngle);
+              let w = Math.sin(inner_angle) * this.innerRadius;
+              let r = Math.cos(inner_angle) * this.innerRadius;
 
+              var q = Math.cos(outer_angle) * r;
+              var p = Math.sin(outer_angle) * r;
 
-              var n_x = (this.outerRadius + this.innerRadius * cosPhi) * cosAngle - cosAngle*this.outerRadius;
-              var n_y = (this.outerRadius + this.innerRadius * cosPhi) * sinAngle - sinAngle*this.outerRadius;
-              var n_z = this.innerRadius * sinPhi;
-              var magn = Math.sqrt(Math.pow(n_x, 2) + Math.pow(n_y,2) + Math.pow(n_z,2));
-              this.normals.push(n_x/magn, n_y/magn, n_z/magn);
+              this.vertices.push(q + outer_x, p + outer_y, w);
 
-              phi += phiStep;
+              this.normals.push(q, p, w);
+
+              this.texCoords.push(slice / this.slices, loop / this.loops);
           }
-          angle += angleStep;
       }
 
       for (let loop = 0; loop < this.loops; loop++) {
         for(let slice = 0; slice < this.slices; slice++) {
-            const first = (loop * (this.slices + 1)) + slice;
-            const second = first + this.slices + 1;
+            const first = loop * (this.slices + 1) + slice;
+            const second = (loop+1) * (this.slices + 1) + slice;
 
-            this.indices.push(first, second + 1, second);
-            this.indices.push(first, first+1, second+1);
+            this.indices.push(first, second, first+1);
+            this.indices.push(second, second+1, first+1);
 
-            this.texCoords.push(1 - (slice / this.slices), 1 - (loop / this.loops));
-            this.texCoords.push(slice/this.slices, 1 - (loop * 1/this.loops));
+            //this.texCoords.push(slice/this.slices, 1 - (loop * 1/this.loops));
         }
       }
 
