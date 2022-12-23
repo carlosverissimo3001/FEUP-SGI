7b697b15be3e0edb54baa37d915ba2f94ce56855
Carlos Verissimo <carlosverissimo3001@gmail.com>
2022-12-23 00:19:00 +0000

    Creating methods for sequence and move

--- tp3/game/orchestrator/game-sequence/MyGameSequence.js
+++ tp3/game/orchestrator/game-sequence/MyGameSequence.js
@@ -3,5 +3,23 @@ import { MyGameMove } from "./MyGameMove.js";
 export class MyGameSequence {
     constructor(scene) {
         this.scene = scene;
+
+        this.moves = [];
+    }
+
+    /**
+     * Add a move to the sequence
+     * @param {MyGameMove} move
+     */
+    addMove(move) {
+        this.moves.push(move);
+    }
+
+    undo() {
+        if (this.moves.length == 0) {
+            console.error("No moves to undo");
+        }
+
+        this.moves.pop();
     }
 }
